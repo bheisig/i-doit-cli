668406f2660cc81a06b171b4d2f4e34cf944badc
Benjamin Heisig <benjamin@heisig.name>
2017-02-16 13:03:41 +0100

    Create racks and put servers inside them
    * Add: Create racks and servers with categories "formfactor" and "location"
    * Add: Put servers into racks
    * Add: As an example use configuration file to create racks and servers (docs/dcim.json)
    * Fix: Cleanup configuration file to create networks (docs/network.json)

--- src/Random.php
+++ src/Random.php
@@ -49,6 +49,11 @@ class Random extends Command {
     protected $subnet;
     protected $subnetID;
 
+    protected $rackIDs = [];
+    protected $rackID;
+    protected $rackRUs;
+    protected $rackPos;
+
     /**
      * Processes some routines before the execution
      *
@@ -84,6 +89,7 @@ class Random extends Command {
         $topics = [
             'countries',
             'subnets',
+            'racks',
             'servers'
         ];
 
@@ -345,18 +351,196 @@ class Random extends Command {
         return $this;
     }
 
+    protected function createRacks() {
+        IO::out('Create racks');
+
+        if (!array_key_exists('amount', $this->config['racks']) ||
+            !is_int($this->config['racks']['amount']) ||
+            $this->config['racks']['amount'] <= 0) {
+            throw new \Exception(
+                'Do not know how many racks to create',
+                400
+            );
+        }
+
+        $rackObjects = [];
+
+        for ($i = 0; $i < $this->config['racks']['amount']; $i++) {
+            $prefix = null;
+
+            if (array_key_exists('prefix', $this->config['racks'])) {
+                $prefix = $this->config['racks']['prefix'];
+            }
+
+            $title = $this->genTitle($prefix);
+
+            $rackObjects[] = [
+                'title' => $title,
+                'type' => $this->config['types']['racks']
+            ];
+        }
+
+        $rackIDs = $this->createObjects($rackObjects);
+
+        $this->logStat('Created rack objects', count($rackIDs));
+
+        if (array_key_exists('rackUnits', $this->config['racks'])) {
+            $amount = $this->config['racks']['rackUnits'];
+
+            if (!is_int($amount) ||
+                $amount <= 0) {
+                throw new \Exception(
+                    'Invalid number of rack units',
+                    400
+                );
+            }
+
+            switch ($amount) {
+                case 0:
+                    IO::out(
+                        'Assign 1 unit to racks'
+                    );
+                    break;
+                default:
+                    IO::out(
+                        'Assign %s units to racks',
+                        $amount
+                    );
+                    break;
+            }
+
+            $this->createCategoryEntries(
+                $rackIDs,
+                'C__CATG__FORMFACTOR',
+                [
+                    // Dialog+: 19"
+                    'formfactor' => 1,
+                    'rackunits' => $amount
+                ]
+            );
+        }
+
+        if (array_key_exists('verticalSlots', $this->config['racks'])) {
+            $amount = $this->config['racks']['verticalSlots'];
+
+            if (!is_int($amount) ||
+                $amount <= 0) {
+                throw new \Exception(
+                    'Invalid number of vertical slots',
+                    400
+                );
+            }
+
+            switch ($amount) {
+                case 0:
+                    IO::out(
+                        'Assign 1 vertical slot to racks'
+                    );
+                    break;
+                default:
+                    IO::out(
+                        'Assign %s vertical slots to racks',
+                        $amount
+                    );
+                    break;
+            }
+
+            $this->createCategoryEntries(
+                $rackIDs,
+                'C__CATS__ENCLOSURE',
+                [
+                    'vertical_slots_rear' => $amount
+                ],
+                false
+            );
+        }
+
+        if (array_key_exists('density', $this->config) &&
+            is_array($this->config['density']) &&
+            array_key_exists('racksPerRoom', $this->config['density'])) {
+            IO::out('Assign racks to rooms');
+
+            if (!is_array($this->config['density']['racksPerRoom']) ||
+                !array_key_exists('min', $this->config['density']['racksPerRoom']) ||
+                !array_key_exists('max', $this->config['density']['racksPerRoom'])) {
+                throw new \Exception(
+                    'Missing mininum and maximum amount of racks per room',
+                    400
+                );
+            }
+
+            $min = $this->config['density']['racksPerRoom']['min'];
+            $max = $this->config['density']['racksPerRoom']['max'];
+
+            if (!is_int($min) ||
+                $min < 1 ||
+                !is_int($max) ||
+                $max < $min) {
+                throw new \Exception(
+                    'Invalid minimum and maximum amount of racks per room',
+                    400
+                );
+            }
+
+            $availableRackIDs = $rackIDs;
+
+            $roomObjects = $this->cmdbObjects->readByType(
+                $this->config['types']['rooms']
+            );
+
+            if (count($roomObjects) === 0) {
+                throw new \Exception(
+                    'No rooms left'
+                );
+            }
+
+            foreach ($roomObjects as $roomObject) {
+                $roomID = (int) $roomObject['id'];
+
+                $amount = mt_rand($min, $max);
+
+                $attributes = [];
+
+                for ($i = 0; $i < $amount; $i++) {
+                    if (count($availableRackIDs) === 0) {
+                        IO::out('All racks assigned to rooms');
+                        break;
+                    }
+
+                    $attributes[] = [
+                        'assigned_object' => array_shift(
+                            $availableRackIDs
+                        )
+                    ];
+                }
+
+                $this->createMultipleCategoryEntriesPerObject(
+                    $roomID,
+                    'C__CATG__OBJECT',
+                    $attributes
+                );
+
+                if (count($availableRackIDs) === 0) {
+                    break;
+                }
+            }
+
+            unset($roomObjects);
+        }
+
+        return $this;
+    }
+
     protected function createServers() {
         IO::out('Create servers');
-
-        if (!is_array($this->config['servers']) ||
-            count($this->config['servers']) === 0) {
-            throw new \Exception('Missing configuration settings for servers', 400);
-        }
 
         if (!array_key_exists('amount', $this->config['servers']) ||
             !is_int($this->config['servers']['amount']) ||
             $this->config['servers']['amount'] <= 0) {
-            throw new \Exception('Do not know how many servers to create', 400);
+            throw new \Exception(
+                'Do not know how many servers to create',
+                400
+            );
         }
 
         $serverObjects = [];
@@ -444,6 +628,67 @@ class Random extends Command {
 
         }
 
+        if (array_key_exists('rackUnits', $this->config['servers'])) {
+            IO::out('Mount servers into racks');
+
+            if (!is_array($this->config['servers']['rackUnits']) ||
+                !array_key_exists('min', $this->config['servers']['rackUnits']) ||
+                !array_key_exists('max', $this->config['servers']['rackUnits'])) {
+                throw new \Exception(
+                    'Missing mininum and maximum rack units per server',
+                    400
+                );
+            }
+
+            $min = $this->config['servers']['rackUnits']['min'];
+            $max = $this->config['servers']['rackUnits']['max'];
+
+            if (!is_int($min) ||
+                $min < 1 ||
+                !is_int($max) ||
+                $max < $min) {
+                throw new \Exception(
+                    'Invalid minimum and maximum rack units per server',
+                    400
+                );
+            }
+
+            $rackObjects = $this->cmdbObjects->readByType(
+                $this->config['types']['racks']
+            );
+
+            $this->rackIDs = [];
+
+            foreach ($rackObjects as $rackObject) {
+                $this->rackIDs[] = (int) $rackObject['id'];
+            }
+
+            if (count($this->rackIDs) === 0) {
+                throw new \Exception(
+                    'There are no racks'
+                );
+            }
+
+            foreach ($serverIDs as $serverID) {
+                $rackUnits = mt_rand($min, $max);
+
+                $this->createCategoryEntry(
+                    $serverID,
+                    'C__CATG__FORMFACTOR',
+                    [
+                        // Dialog+: 19"
+                        'formfactor' => 1,
+                        'rackunits' => $rackUnits
+                    ]
+                );
+
+                $this->assignHostToRack(
+                    $serverID,
+                    $rackUnits
+                );
+            }
+        }
+
         return $this;
     }
 
@@ -470,7 +715,10 @@ class Random extends Command {
             if (!is_float($this->config['density']['subnets']) ||
                 $this->config['density']['subnets'] <= 0 ||
                 $this->config['density']['subnets'] > 1) {
-                throw new \Exception('Subnet density must be a float between greater than 0 and lower equal 1');
+                throw new \Exception(
+                    'Subnet density must be a float between greater than 0 and lower equal 1',
+                    400
+                );
             }
 
             $density = round($this->config['density']['subnets'] * 100);
@@ -487,6 +735,88 @@ class Random extends Command {
         }
 
         return $next;
+    }
+
+    protected function assignHostToRack($objectID, $neededRUs) {
+        if (!isset($this->rackID)) {
+            if (count($this->rackIDs) === 0) {
+                throw new \Exception(
+                    'No space left in racks'
+                );
+            }
+
+            $this->rackID = array_shift($this->rackIDs);
+
+            $rack = $this->cmdbCategory->readFirst(
+                $this->rackID,
+                'C__CATG__FORMFACTOR'
+            );
+
+            $this->rackRUs = $rack['rackunits'];
+
+            $this->rackPos = 0;
+        }
+
+        // @todo This is not the right way to calculate rack unit density:
+        if (array_key_exists('density', $this->config) &&
+            array_key_exists('rackUnits', $this->config['density'])) {
+            if (!is_float($this->config['density']['rackUnits']) ||
+                $this->config['density']['rackUnits'] <= 0 ||
+                $this->config['density']['rackUnits'] > 1) {
+                throw new \Exception(
+                    'Rack unit density must be a float between greater than 0 and lower equal 1',
+                    400
+                );
+            }
+
+            $density = round($this->config['density']['rackUnits'] * 100);
+            $dice = mt_rand(1, 100);
+
+            if ($density < $dice) {
+                $this->rackPos++;
+            }
+        }
+
+        // No space left:
+        if ($this->rackPos >= $this->rackRUs) {
+            IO::out(
+                'No space left in rack #%s',
+                $this->rackID
+            );
+
+            $this->rackID = null;
+
+            return $this->assignHostToRack($objectID, $neededRUs);
+        }
+
+
+        if ($this->rackPos + $neededRUs >= $this->rackRUs) {
+            IO::out(
+                'Not enough space left in rack #%s',
+                $this->rackID
+            );
+
+            $this->rackID = null;
+
+            return $this->assignHostToRack($objectID, $neededRUs);
+        }
+
+        $this->createCategoryEntry(
+            $objectID,
+            'C__CATG__LOCATION',
+            [
+                'parent' => $this->rackID,
+                // Use front and back side:
+                'insertion' => 2,
+                // Horizontal rack unit:
+                'option' => 2,
+                'pos' => $this->rackPos
+            ]
+        );
+
+        $this->rackPos += $neededRUs;
+
+        return $this;
     }
 
     protected function createObjects($objects) {
@@ -551,7 +881,7 @@ class Random extends Command {
 
     protected function createCategoryEntry($objectID, $categoryConst, $attributes, $isGlobal = true) {
         IO::out(
-            'Create one entry into category "%s" for object %s',
+            'Create one entry into category "%s" for object #%s',
             $categoryConst,
             $objectID
         );
@@ -568,8 +898,8 @@ class Random extends Command {
 
         IO::out(
             'Create same entry into category "%s" for %s objects',
-            $count,
-            $categoryConst
+            $categoryConst,
+            $count
         );
 
         if ($this->config['limitBatchRequests'] > 0 &&
@@ -625,7 +955,7 @@ class Random extends Command {
         $count = count($attributes);
 
         IO::out(
-            'Create %s entries into category "%s" for object %s',
+            'Create %s entries into category "%s" for object #%s',
             $count,
             $categoryConst,
             $objectID
