7834c6d79e14a2e292c2862c31fd39b66e58a6a1
Benjamin Heisig <benjamin@heisig.name>
2018-12-04 17:02:40 +0100

    Use logger wisely

--- src/Command/Show.php
+++ src/Command/Show.php
@@ -26,15 +26,13 @@ declare(strict_types=1);
 
 namespace bheisig\idoitcli\Command;
 
-use bheisig\cli\IO;
-
 /**
  * Command "show"
  */
 class Show extends Command {
 
     /**
-     * Processes some routines before the execution
+     * Process some routines before executing command
      *
      * @return self Returns itself
      *
@@ -54,13 +52,19 @@ class Show extends Command {
     }
 
     /**
-     * Executes the command
+     * Execute command
      *
      * @return self Returns itself
      *
      * @throws \Exception on error
      */
     public function execute(): self {
+        $this->log->printAsMessage()
+            ->info(
+                $this->getDescription()
+            )
+            ->printEmptyLine();
+
         $query = $this->getQuery();
 
         $objectID = 0;
@@ -84,24 +88,25 @@ class Show extends Command {
                     $objectID = (int) $objects[0]['id'];
                     break;
                 default:
-                    IO::err('Found %s objects', count($objects));
-                    IO::err('');
+                    $this->log->printAsMessage()
+                        ->debug('Found %s objects', count($objects))
+                        ->printEmptyLine();
 
                     foreach ($objects as $object) {
-                        IO::err(
+                        $this->log->printAsOutput()->info(
                             '%s: %s',
                             $object['id'],
                             $object['title']
                         );
                     }
 
-                    IO::err('');
+                    $this->log->printAsMessage()->printEmptyLine();
 
                     while (true) {
-                        $objectID = (int) IO::in('Which object do you like to show? [identifier]');
+                        $objectID = (int) $this->userInteraction->askQuestion('Object?');
 
                         if ($objectID <= 0) {
-                            IO::err('Please try again.');
+                            $this->log->printAsMessage()->warning('Please try again.');
                         } else {
                             break;
                         }
@@ -112,13 +117,14 @@ class Show extends Command {
 
         $object = $this->useIdoitAPI()->getCMDBObject()->load($objectID);
 
-        IO::out('Title: %s', $object['title']);
-        IO::out('ID: %s', $object['id']);
-        IO::out('Type: %s', $object['type_title']);
-        IO::out('SYS-ID: %s', $object['sysid']);
-        IO::out('CMDB status: %s', $object['cmdb_status_title']);
-        IO::out('Created: %s', $object['created']);
-        IO::out('Updated: %s', $object['updated']);
+        $this->log->printAsOutput()
+            ->info('Title: %s', $object['title'])
+            ->info('ID: %s', $object['id'])
+            ->info('Type: %s', $object['type_title'])
+            ->info('SYS-ID: %s', $object['sysid'])
+            ->info('CMDB status: %s', $object['cmdb_status_title'])
+            ->info('Created: %s', $object['created'])
+            ->info('Updated: %s', $object['updated']);
 
         $categoryTypes = ['catg', 'cats'];
 
@@ -141,23 +147,23 @@ class Show extends Command {
                     continue;
                 }
 
-                IO::err('');
+                $this->log->printAsMessage()->printEmptyLine();
 
                 switch (count($category['entries'])) {
                     case 0:
-                        IO::out(
+                        $this->log->printAsMessage()->info(
                             'No entries in category "%s"',
                             $category['title']
                         );
                         continue 2;
                     case 1:
-                        IO::out(
+                        $this->log->printAsMessage()->info(
                             '1 entry in category "%s":',
                             $category['title']
                         );
                         break;
                     default:
-                        IO::out(
+                        $this->log->printAsMessage()->info(
                             '%s entries in category "%s":',
                             count($category['entries']),
                             $category['title']
@@ -168,12 +174,12 @@ class Show extends Command {
                 try {
                     $categoryInfo = $this->cache->getCategoryInfo($category['const']);
                 } catch (\Exception $e) {
-                    IO::err($e->getMessage());
+                    $this->log->printAsMessage()->notice($e->getMessage());
                     continue;
                 }
 
                 foreach ($category['entries'] as $entry) {
-                    IO::err('');
+                    $this->log->printAsMessage()->printEmptyLine();
 
                     foreach ($entry as $attribute => $value) {
                         if (in_array($attribute, ['id', 'objID'])) {
@@ -211,7 +217,7 @@ class Show extends Command {
                             $value = '-';
                         }
 
-                        IO::out(
+                        $this->log->printAsOutput()->info(
                             '%s: %s',
                             $categoryInfo['properties'][$attribute]['title'],
                             $value
@@ -242,6 +248,56 @@ Examples:
 1) %1$s %2$s myserver
 2) %1$s %2$s "My Server"
 3) %1$s %2$s 42',
+            $this->config['composer']['extra']['name'],
+            $this->getName(),
+            $this->getDescription()
+        );
+
+        $this->log->info(
+            <<< EOF
+%3\$s
+
+<strong>USAGE</strong>
+    \$ %1\$s %2\$s [OPTIONS] [OBJECT]
+
+<strong>ARGUMENTS</strong>
+    OBJECT              <dim>Object title or numeric identifier</dim>
+
+<strong>COMMON OPTIONS</strong>
+    -c <u>FILE</u>,            <dim>Include settings stored in a JSON-formatted</dim>
+    --config=<u>FILE</u>       <dim>configuration file FILE; repeat option for more</dim>
+                        <dim>than one FILE</dim>
+    -s <u>KEY=VALUE</u>,       <dim>Add runtime setting KEY with its VALUE; separate</dim>
+    --setting=<u>KEY=VALUE</u> <dim>nested keys with ".", for example "key1.key2=123";</dim>
+                        <dim>repeat option for more than one KEY</dim>
+
+    --no-colors         <dim>Do not print colored messages</dim>
+    -q, --quiet         <dim>Do not output messages, only errors</dim>
+    -v, --verbose       <dim>Be more verbose</dim>
+
+    -h, --help          <dim>Print this help or information about a</dim>
+                        <dim>specific command</dim>
+    --version           <dim>Print version information</dim>
+
+    -y, --yes           <dim>No user interaction required; answer questions</dim>
+                        <dim>automatically with default values</dim>
+
+<strong>EXAMPLES</strong>
+    <dim># %3\$s by its title:</dim>
+    \$ %1\$s %2\$s mylittleserver
+
+    <dim># …or by its numeric identifier:</dim>
+    \$ %1\$s %2\$s 42
+    
+    <dim>Handle spaces in title with care:</dim>
+    \$ %1\$s %2\$s "My little server"
+    \$ %1\$s %2\$s My\\ little\\ server
+
+    <dim># If argument OBJECT is omitted you'll be asked for it:</dim>
+    \$ %1\$s %2\$s
+    Object? host01.example.com
+EOF
+            ,
             $this->config['composer']['extra']['name'],
             $this->getName(),
             $this->getDescription()
